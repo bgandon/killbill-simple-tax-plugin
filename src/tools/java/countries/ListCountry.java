cbf320a33dde473d2846adc68a5d6086ddc201f7
Benjamin Gandon <benjamin@gandon.org>
2015-10-30 14:56:05 +0100

    Added missing license header

--- src/tools/java/countries/ListCountry.java
+++ src/tools/java/countries/ListCountry.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015 Benjamin Gandon
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package countries;
 
 import static java.lang.System.out;
@@ -15,7 +31,7 @@ import java.util.TreeMap;
  * Inspired by <a
  * href="http://www.mkyong.com/java/display-a-list-of-countries-in-java/"
  * >Display a list of countries in Java</a>.
- * 
+ *
  * @author Benjamin Gandon
  */
 @SuppressWarnings("javadoc")
